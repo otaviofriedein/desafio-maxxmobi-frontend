0ddbed3f7ab509b4b34b1be0f81bf64d03f7483e
Otávio Friedein <otavio.friedein@pcdf.df.gov.br>
2024-05-06 16:21:50 -0300

    Refatorando CandidatoService

--- src/app/service/candidato/candidato.service.ts
+++ src/app/service/candidato/candidato.service.ts
@@ -1,32 +1,32 @@
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, tap, throwError } from 'rxjs';
 import { Candidato } from '../../models/candidato';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CandidatoService {
 
-  url = 'http://localhost:8081/candidato'; 
+  url = 'http://localhost:8081/candidato';
 
   constructor(
     private httpClient: HttpClient,
-    private _snackBar: MatSnackBar
+    private snackBar: MatSnackBar
   ) { }
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   }
 
-  getCandidatos(params:string): Observable<Candidato[]> {
+  getCandidatos(params: string): Observable<Candidato[]> {
     return this.httpClient.get<Candidato[]>(this.url + params).pipe(
       catchError(error => {
         this.showErrorSnackBar('Não foi possível carregar os candidatos');
         return throwError(() => error);
-      })
-    );
+      }));
   }
 
   getCandidatoById(id: number): Observable<Candidato> {
@@ -34,21 +34,27 @@ export class CandidatoService {
       catchError(error => {
         this.showErrorSnackBar('Não foi possível carregar o candidato de ID: ' + id);
         return throwError(() => error);
-      })
-    );
+      }));
   }
 
-  saveCandidato(candidato: Candidato): Observable<Candidato> {
+  createCandidato(candidato: Candidato): Observable<Candidato> {
     return this.httpClient.post<Candidato>(this.url, JSON.stringify(candidato), this.httpOptions).pipe(
+      tap((response) => {
+        this.showSuccessSnackBarAndReload('Candidato criado com sucesso');
+        return response;
+      }),
       catchError(error => {
         this.showErrorSnackBar('Não foi possível criar o candidato');
         return throwError(() => error);
-      })
-    );
+      }));
   }
 
   updateCandidato(candidato: Candidato): Observable<Candidato> {
     return this.httpClient.put<Candidato>(this.url + '/' + candidato.id, JSON.stringify(candidato), this.httpOptions).pipe(
+      tap((response) => {
+        this.showSuccessSnackBarAndReload('Candidato atualizado com sucesso');
+        return response;
+      }),
       catchError(error => {
         this.showErrorSnackBar('Não foi possível altualizar o candidato!');
         return throwError(() => error);
@@ -57,19 +63,24 @@ export class CandidatoService {
   }
 
   deleteCandidato(id: number) {
-    return this.httpClient.delete<Candidato>(this.url + '/' + id, this.httpOptions).pipe(
+    return this.httpClient.delete(this.url + '/' + id, this.httpOptions).pipe(
+      tap((response) => {
+        this.showSuccessSnackBarAndReload('Candidato excluído com sucesso');    
+        return response;
+      }),
       catchError(error => {
         this.showErrorSnackBar('Não foi possível deletar o candidato');
         return throwError(() => error);
       })
     );
   }
-  
+
+  private showSuccessSnackBarAndReload(mensagem: string) {    
+    this.snackBar.open('Candidato excluído com sucesso', '', { duration: 3000 });
+    setTimeout(() => { location.reload(); }, 3000);     
+  }
+
   private showErrorSnackBar(mensagem: string) {
-    this._snackBar.open(mensagem, 'Fechar', {
-      duration: 3000,
-      verticalPosition: 'bottom',
-      horizontalPosition: 'center'
-    });
+    this.snackBar.open(mensagem, 'Fechar', { duration: 3000 });
   }
 }
