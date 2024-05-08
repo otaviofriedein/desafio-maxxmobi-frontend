36580424801d15ad724c75be0518034ff7239e4a
otaviofriedein <friedein.otavio@gmail.com>
2024-05-08 12:37:35 -0300

    Refatorando CandidatoService

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { Candidato,  } from '../../models/candidato';
+import { Component, OnInit } from '@angular/core';
+import { Candidato, } from '../../models/candidato';
 import { CandidatoService } from '../../service/candidato/candidato.service';
 import { CandidatoFilters } from '../../models/candidatoFilters';
 import { MatDialog } from '@angular/material/dialog';
 import { UtilService } from '../../service/utils/util.service';
 import { CandidatoComponent } from './candidato/candidato.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
@@ -12,75 +13,87 @@ import { CandidatoComponent } from './candidato/candidato.component';
   styleUrl: './home.component.css'
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   tableColumns: string[] = ['id', 'nome', 'nascimento', 'sexo', 'nota', 'logradouro', 'bairro', 'cidade', 'uf', 'options'];
-  
+
   candidato = {} as Candidato;
   candidatoFilters = {} as CandidatoFilters;
   candidatos: Candidato[] = [];
 
   constructor(
-    private candidatoService: CandidatoService, 
+    private candidatoService: CandidatoService,
     private util: UtilService,
-    public dialog: MatDialog
-  ) {} 
+    private snackBar: MatSnackBar,
+    public dialog: MatDialog,
+  ) { }
 
-  ngOnInit = () => this.getAllCandidatos();
-  
+  ngOnInit() {
+    this.getAllCandidatos();
+  }
+
   getAllCandidatos() {
     let params = this.getFilterCandidato();
 
     this.candidatoService.getCandidatos(params).subscribe((candidatos: Candidato[]) => {
       this.candidatos = candidatos;
     });
-  }  
+  }
 
-  openDialogToCreateCandidato() {  
+  openDialogToCreateCandidato() {
     let candidatoComponent = this.dialog.open(CandidatoComponent);
-      
-      candidatoComponent.componentInstance.save.subscribe((newCandidato) => {
-        this.createCandidato(newCandidato);
-      });  
+
+    candidatoComponent.componentInstance.save.subscribe((newCandidato) => {
+      this.createCandidato(newCandidato);
+    });
   }
 
-  openDialogToViewCandidato(id:number) {  
+  openDialogToViewCandidato(id: number) {
     this.candidatoService.getCandidatoById(id).subscribe((candidato: Candidato) => {
       let candidatoComponent = this.dialog.open(CandidatoComponent);
       this.candidato = candidato;
-      
+
       candidatoComponent.componentInstance.candidato = this.candidato;
-      candidatoComponent.componentInstance.readonly = true;       
-    })   
+      candidatoComponent.componentInstance.readonly = true;
+    })
   }
 
-  openDialogToUpdateCandidato(id:number) {
+  openDialogToUpdateCandidato(id: number) {
     this.candidatoService.getCandidatoById(id).subscribe((candidato: Candidato) => {
       let candidatoComponent = this.dialog.open(CandidatoComponent);
       this.candidato = candidato;
-      
+
       candidatoComponent.componentInstance.candidato = this.candidato;
       candidatoComponent.componentInstance.save.subscribe((editedCandidato) => {
         this.updateCandidato(editedCandidato);
-      });  
-    })        
-  }  
+      });
+    })
+  }
 
   clearFilterCandidato = () => location.reload();
 
-  createCandidato = (candidato: Candidato) => 
-    this.candidatoService.createCandidato(candidato);
-  
-  updateCandidato = (candidato: Candidato) => 
-    this.candidatoService.updateCandidato(candidato);
-  
-  deleteCandidato = (id:number) => 
-    this.candidatoService.deleteCandidato(id);  
+  createCandidato = (candidato: Candidato) =>
+    this.candidatoService.createCandidato(candidato).subscribe((response => {
+      this.showSuccessSnackBarAndReload('Candidato criado com sucesso');
+      return response;
+    }));
 
-  getFilterCandidato(){
+  updateCandidato = (candidato: Candidato) =>
+    this.candidatoService.updateCandidato(candidato).subscribe((response => {
+      this.showSuccessSnackBarAndReload('Candidato atualizado com sucesso');
+      return response;
+    }));
+
+  deleteCandidato = (id: number) =>
+    this.candidatoService.deleteCandidato(id).subscribe((response => {
+      this.showSuccessSnackBarAndReload('Candidato excluído com sucesso');
+      return response;
+    }));
+
+  getFilterCandidato() {
     let params = '';
 
-    if (this.candidatoFilters.nome != undefined) params += `nome=${this.candidatoFilters.nome}&`;
+    if (this.candidatoFilters.nome != undefined && this.candidatoFilters.nome != '') params += `nome=${this.candidatoFilters.nome}&`;
     if (this.candidatoFilters.sexo != undefined) params += `sexo=${this.candidatoFilters.sexo}&`;
     if (this.candidatoFilters.nota != undefined) params += `nota=${this.candidatoFilters.nota}&`;
     if (this.candidatoFilters.nascimento != undefined) {
@@ -89,5 +102,10 @@ export class HomeComponent {
     if (this.candidatoFilters.sortBy != undefined) params += `sortBy=${this.candidatoFilters.sortBy.value}&order=${this.candidatoFilters.sortBy.direction}`;
 
     return (params.length > 0) ? '?' + params : params;
-  }  
+  }
+
+  showSuccessSnackBarAndReload(mensagem: string) {
+    this.snackBar.open(mensagem, '', { duration: 3000 });
+    setTimeout(() => { location.reload(); }, 3000);
+  }
 }
--- src/app/service/candidato/candidato.service.ts
+++ src/app/service/candidato/candidato.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, tap, throwError } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { Candidato } from '../../models/candidato';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { API_URL } from '../../api.config';
@@ -40,10 +40,6 @@ export class CandidatoService {
 
   createCandidato(candidato: Candidato): Observable<Candidato> {
     return this.httpClient.post<Candidato>(`${API_URL}${this.resource}`, JSON.stringify(candidato), this.httpOptions).pipe(
-      tap((response) => {
-        this.showSuccessSnackBarAndReload('Candidato criado com sucesso');
-        return response;
-      }),
       catchError(error => {
         this.showErrorSnackBar('Não foi possível criar o candidato');
         return throwError(() => error);
@@ -52,10 +48,6 @@ export class CandidatoService {
 
   updateCandidato(candidato: Candidato): Observable<Candidato> {
     return this.httpClient.put<Candidato>(`${API_URL}${this.resource}/${candidato.id}`, JSON.stringify(candidato), this.httpOptions).pipe(
-      tap((response) => {
-        this.showSuccessSnackBarAndReload('Candidato atualizado com sucesso');
-        return response;
-      }),
       catchError(error => {
         this.showErrorSnackBar('Não foi possível altualizar o candidato!');
         return throwError(() => error);
@@ -65,20 +57,11 @@ export class CandidatoService {
 
   deleteCandidato(id: number) {
     return this.httpClient.delete(`${API_URL}${this.resource}/${id}`, this.httpOptions).pipe(
-      tap((response) => {
-        this.showSuccessSnackBarAndReload('Candidato excluído com sucesso');    
-        return response;
-      }),
       catchError(error => {
         this.showErrorSnackBar('Não foi possível deletar o candidato');
         return throwError(() => error);
       })
     );
-  }
-
-  private showSuccessSnackBarAndReload(mensagem: string) {    
-    this.snackBar.open('Candidato excluído com sucesso', '', { duration: 3000 });
-    setTimeout(() => { location.reload(); }, 3000);     
   }
 
   private showErrorSnackBar(mensagem: string) {
