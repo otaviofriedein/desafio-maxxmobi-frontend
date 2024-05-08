c89e185cff7b1df155e77942d4a54dceabdd8f50
Otávio Friedein <otavio.friedein@pcdf.df.gov.br>
2024-05-08 10:07:33 -0300

    Fix: testes unitários

--- src/app/service/autenticacao/autenticacao.service.spec.ts
+++ src/app/service/autenticacao/autenticacao.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
 import { AutenticacaoService } from './autenticacao.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AutenticacaoService', () => {
   let service: AutenticacaoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ 
+      imports:[
+      HttpClientModule
+    ]});
     service = TestBed.inject(AutenticacaoService);
   });
 
